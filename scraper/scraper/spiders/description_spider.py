c8d272783f0166e721cb7e5ac79fbd28b71feb66
li-jiang-yan <professional.jiangyan_li@outlook.com>
2026-03-16 10:05:31 +0800

    Changed scraping code for scraping with multiple spiders

--- scraper/scraper/spiders/description_spider.py
+++ scraper/scraper/spiders/description_spider.py
@@ -1,47 +1,47 @@
 import scrapy
-from scrapy.signalmanager import dispatcher
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
 from bs4 import BeautifulSoup
 
 class Spider(scrapy.Spider):
     name = "description"
 
-    def __init__(self, start_urls):
+    def __init__(self, start_urls, results=None):
         super().__init__()
         self.start_urls = start_urls
+        self.results = results if results is not None else []
 
     def parse(self, response):
-        yield BeautifulSoup(response.body, "html.parser").select_one("section.description").prettify()
-
-class ScraperRun:
-    def __init__(self, start_urls):
-        self.start_urls = start_urls
-        self.items = []
-        dispatcher.connect(self.crawler_results, signal=scrapy.signals.item_scraped)
-
-    def crawler_results(self, signal, sender, item, response, spider):
-        self.items.append(item)
-
-    def run_spider(self):
-        settings = get_project_settings()
-        process = CrawlerProcess(settings=settings)
-        process.crawl(Spider, start_urls=self.start_urls)
-        process.start()
-        return self.items
+        self.results.append(BeautifulSoup(response.body, "html.parser").select_one("section.description").prettify())
 
 if __name__ == "__main__":
     import os
     import json
 
+    from scrapy.crawler import CrawlerRunner
+    from scrapy.utils.project import get_project_settings
+    from scrapy.utils.reactor import install_reactor
+
+    # Install the reactor BEFORE importing twisted.internet.reactor
+    install_reactor("twisted.internet.asyncioreactor.AsyncioSelectorReactor")
+
+    from twisted.internet import reactor, defer
+
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    results = []
+
     with open("../../posts.json", "r", encoding="utf-8") as f:
         posts = json.load(f)
         soups = list(BeautifulSoup(html_doc, "html.parser") for html_doc in posts)
         start_urls = list(soup.select_one("a.base-card__full-link").get("href") for soup in soups)
 
-    scraper = ScraperRun(start_urls=start_urls)
-    results = scraper.run_spider()
+    @defer.inlineCallbacks
+    def crawl():
+        runner = CrawlerRunner(get_project_settings())
+        yield runner.crawl(Spider, start_urls=start_urls, results=results)
+        reactor.stop()
+
+    crawl()
+    reactor.run()
 
     with open("../../descriptions.json", "w", encoding="utf-8") as f:
         json.dump(results, f, indent=4)
--- scraper/scraper/spiders/posts_spider.py
+++ scraper/scraper/spiders/posts_spider.py
@@ -1,46 +1,43 @@
-from itertools import chain
 from urllib.parse import quote
 import scrapy
-from scrapy.signalmanager import dispatcher
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
 from bs4 import BeautifulSoup
 
 class Spider(scrapy.Spider):
     name = "posts"
 
-    def __init__(self, keywords, location, number):
+    def __init__(self, keywords, location, number, results=None):
         super().__init__()
         self.start_urls = list(f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={quote(keywords)}&location={quote(location)}&start={start}" for start in range(0, int(number), 25))
+        self.results = results if results is not None else []
 
     def parse(self, response):
-        yield list(BeautifulSoup(html_doc, "html.parser").prettify() for html_doc in response.css("div.base-card").getall())
-
-class ScraperRun:
-    def __init__(self, keywords="Computer Science", location="Singapore", number=10):
-        self.keywords = keywords
-        self.location = location
-        self.number = number
-        self.items = []
-        dispatcher.connect(self.crawler_results, signal=scrapy.signals.item_scraped)
-
-    def crawler_results(self, signal, sender, item, response, spider):
-        self.items.append(item)
-
-    def run_spider(self):
-        settings = get_project_settings()
-        process = CrawlerProcess(settings=settings)
-        process.crawl(Spider, keywords=self.keywords, location=self.location, number=self.number)
-        process.start()
-        return list(chain.from_iterable(self.items))
+        self.results.extend(BeautifulSoup(html_doc, "html.parser").prettify() for html_doc in response.css("div.base-card").getall())
 
 if __name__ == "__main__":
     import os
     import json
 
+    from scrapy.crawler import CrawlerRunner
+    from scrapy.utils.project import get_project_settings
+    from scrapy.utils.reactor import install_reactor
+
+    # Install the reactor BEFORE importing twisted.internet.reactor
+    install_reactor("twisted.internet.asyncioreactor.AsyncioSelectorReactor")
+
+    from twisted.internet import reactor, defer
+
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    scraper = ScraperRun(keywords="Computer Science", location="Singapore", number=100)
-    results = scraper.run_spider()
+
+    results = []
+
+    @defer.inlineCallbacks
+    def crawl():
+        runner = CrawlerRunner(get_project_settings())
+        yield runner.crawl(Spider, keywords="Computer Science", location="Singapore", number=100, results=results)
+        reactor.stop()
+
+    crawl()
+    reactor.run()
 
     with open("../../posts.json", "w", encoding="utf-8") as f:
         json.dump(results, f, indent=4)
